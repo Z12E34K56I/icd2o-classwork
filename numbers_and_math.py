7e5ea0b49022aed4e8d7f6fa66305085f35ff8e2
Z12E34K56I <yihan.he26@ycdsbk12.ca>
2024-11-11 13:48:14 -0500

    Create numbers_and_math.py

---
+++ numbers_and_math.py
@@ -0,0 +1,23 @@
+a = 5  
+b = 7.1234  
+
+print(a + b)
+
+pizzas = 32 / 10
+print(pizzas)
+
+
+flowers = int(input("Enter number of flowers: "))
+bushes = int(input("Numebr of bushes: "))
+
+plants = flowers + bushes
+
+print(f"You have {plants} plants")apples = 5
+price_per_apple = 0.99
+total = apples * price_per_apple
+print(total)
+
+apples = int(input("Number of apples: "))
+price_per_apple = float(input("Price per apple: $"))
+total = apples * price_per_apple
+print(total)
