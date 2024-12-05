88054c6f22aecf45eb23e88fc7e5ac2e282edde6
Z12E34K56I <yihan.he26@ycdsbk12.ca>
2024-12-05 13:24:06 -0500

    Update while_loop_practice.py

--- while_loop_practice.py
+++ while_loop_practice.py
@@ -1,1 +1,36 @@
+Everyone
+1.count=0
+while count<=10:
+    print(count)
+    count+=1
+2.count=30
+while count<=40:
+    if count==35:
+        count+=1
+    print(count)
+    count+=1
+
+50%
+1.x=100
+while x>=50:
+    print(x)
+    x-=3
+2.start=int(input("start:"))
+end=int(input("end:"))
+by=int(input("going up by:"))
+count=start
+while count<=end:
+    print(count)
+    count+=by
+
+20%
+a=int(input("First number:"))
+b=int(input("Second number:"))
+c=int(input("Third number:"))
+d=int(input("Fourth number:"))
+e=int(input("Fifth number:"))
+Total=a+b+c+d+e
+print(Total)
+
+10%
 
