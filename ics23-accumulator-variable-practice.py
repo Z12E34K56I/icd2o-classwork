10a9d6eb1509e3aac292fb1c35486b6e22c52c8a
Zeki(Yihan) <yihan.he26@ycdsbk12.ca>
2024-12-11 12:34:50 -0500

    Update ics23-accumulator-variable-practice.py

--- ics23-accumulator-variable-practice.py
+++ ics23-accumulator-variable-practice.py
@@ -9,3 +9,27 @@ while count<=10:
 print(total)
 
 2.100~200
+count=100
+total=0
+while count<=200:
+    print(count)
+    total+=count
+    count+=1
+print(total)
+
+3.(200~300)-(100~200)
+X1=100
+Y1=0
+X2=200
+Y2=0
+while X1<=200:
+    print(X1)
+    Y1+=X1
+    X1+=1
+while X2<=300:
+    print(X2)
+    Y2+=X2
+    X2+=1
+    print(Y2-Y1)
+    
+4.1000~10000/5
